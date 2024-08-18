b45edae6d4104af0006ed537ea50d00475d8a3f9
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-08-18 22:21:10 +0300

    part 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
       <Footer />
       <WelcomeMessage />
