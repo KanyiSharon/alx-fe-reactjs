47b33eb1afcf24d2e96c691d1b0447e81758967f
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-08-25 18:38:10 +0300

    made changes

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,14 +1,18 @@
-const UserProfile=(props)=>{
-    return (
-      <div
-        style={{ border: "1px solid grey", padding: "10px", margin: "10px" }}
-      >
-        <h2 style={{ color: "blue" }}>{props.name}</h2>
-        <p>
-          Age: <span style={{fontWeight:'blue'}}>{props.age}</span>
-        </p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-}
+import React, { useContext } from "react";
+import UserContext from "./components/UserContext";
+
+const UserProfile = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <div style={{ border: "1px solid grey", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{user.name}</h2>
+      <p>
+        Age: <span style={{ color: "blue" }}>{user.age}</span>
+      </p>
+      <p>Bio: {user.bio}</p>
+    </div>
+  );
+};
+
 export default UserProfile;
