e79d4566327ae362b23448743b8f704aa468ac91
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-08-25 18:00:02 +0300

    made changes to app.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,14 +1,15 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import Counter from './components/Counter';
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
+import userContext from "../../alx-react-app-props/src/components/UserContext";
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,12 +22,9 @@ function App() {
       <WelcomeMessage />
       <Header />
       <Counter />
-     
-        
-     
-     
-  </>
+      <userContext />
+    </>
   );
 }
 
-export default App
+export default App;
