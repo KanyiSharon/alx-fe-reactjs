2032d2ca80153b0e58cc2a83bca8176c90f42e86
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-08-25 18:13:17 +0300

    made changes

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,15 @@
 import ProfilePage from "./ProfilePage";
+import userContext from "./components/UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return <ProfilePage userData={userData} />;
 }
+return (
+  <userContext.Provider value={userData}>
+    <ProfilePage />
+  </userContext.Provider>
+)
 
 export default App;
