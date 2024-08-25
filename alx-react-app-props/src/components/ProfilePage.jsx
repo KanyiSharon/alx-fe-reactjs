f5b75d182b1232f4346f7577fdd0b8364e408ca3
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-08-25 16:29:26 +0300

    made changes

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import UserInfo from "alx-react-app-propssrccomponentsUserInfo.jsx";
+
+function ProfilePage({ userData }) {
+  return <UserInfo userData={userData} />;
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,7 @@
+import UserDetails from "alx-react-app-propssrccomponentsUserDetails.jsx";
+
+function UserInfo({ userData }) {
+  return <UserDetails userData={userData} />;
+}
+
+export default UserInfo;
