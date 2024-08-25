ea3fb8ccde91a11586a16161cb878c7fe435e764
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-08-25 14:21:32 +0300

    made changes to header file

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,8 +1,10 @@
-function Header(){
-    return (
-      <header>
-        <h1>My Favorite Cities</h1>
-      </header>
-    );
+function Header() {
+  return (
+    <header
+      style={{ backgroundColor: "navy", color: "white", textAlign: "centre" }}
+    >
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
 }
 export default Header;
