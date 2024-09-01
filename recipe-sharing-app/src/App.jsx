64ed1e4f3b48fce2977fcf2d6ea60a562690c828
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-09-01 21:15:20 +0300

    made chanegs

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,28 @@
-import './App.css';
-import AddRecipeForm from "recipe-sharing-appsrccomponentsAddRecipeForm.jsx";
-import  RecipeList  from './components/RecipeList';
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
+import SearchBar from "./components/SearchBar";
+import FavoritesList from "./components/FavoritesList"; // Importing FavoritesList
+import RecommendationsList from "./components/RecommendationsList"; // Importing RecommendationsList
 
-function App() {
+const App = () => {
   return (
-    <>
-    <AddRecipeForm/>
-    <RecipeList/>
-    </>
+    <Router>
+      <div className="app-container">
+        <h1>Recipe Manager</h1>
+        <SearchBar />
+        <AddRecipeForm />
+        <FavoritesList /> {/* Display user's favorite recipes */}
+        <RecommendationsList /> {/* Display personalized recommendations */}
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
-export default App
+export default App;
