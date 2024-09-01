2b6f300748d787b1f3c07e82d23da26a39a091c1
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-09-01 21:59:25 +0300

    made changes

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,6 @@
 import useRecipeStore from "./recipeStore";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
@@ -6,11 +8,15 @@ const RecipeList = () => {
     <div>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>{" "}
+            {/* Link to the recipe detail page */}
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
     </div>
   );
 };
+
 export default RecipeList;
