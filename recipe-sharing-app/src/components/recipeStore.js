f2c1c693189b0928f2a61cde81a546cc2641b658
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-09-01 21:27:30 +0300

    made changes

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,17 @@
 import create from "zustand";
 
- const useRecipeStore = create((set) => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (recipeId) => {
+    set((state) => {
+      const newRecipes = state.recipes.filter(
+        (recipe) => recipe.id !== recipeId
+      );
+      return { recipes: newRecipes };
+    });
+  },
 }));
 export default useRecipeStore;
