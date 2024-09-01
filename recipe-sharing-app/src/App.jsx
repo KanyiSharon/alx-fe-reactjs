c02b332b36397d15bd2404791e4f19545dac5147
KanyiSharon <sharon.kanyi@strathmore.edu>
2024-09-01 20:39:18 +0300

    made changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { AddRecipeForm } from "recipe-sharing-appsrccomponentsAddRecipeForm.jsx";
 import { RecipeList } from "recipe-sharing-appsrccomponentsRecipeList.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
